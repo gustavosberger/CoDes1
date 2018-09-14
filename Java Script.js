df6cf899e78870e04b6ff9b365dcebed0f138007
LuigiPortugal <38053747+LuigiPortugal@users.noreply.github.com>
2018-09-14 13:31:40 -0300

    arquivos Firebase (json) e Java (js)
    só raw arquive sem nenhuma mudança

---
+++ Java Script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+  let db = new DB('https://co-design-grupo-2-projeto-1.firebaseio.com/')
+
+  let tpName = new TP('template-name')
+
+  let tpGoals = new TP('template-goals')
+
+  let params = new URLSearchParams(window.location.search)
+
+  let url = '/' + params.get('category') + '/courses/' + params.get('course') + '/'
+
+  db.download(url, function(data) {
+    let name = document.querySelector('.name')
+    name.innerHTML = tpName.generate({'name': data['name']})
+
+    let goals = document.querySelector('.goals')
+    goals.innerHTML = tpGoals.generate({'goals': data['goals']})
+  })
+})
