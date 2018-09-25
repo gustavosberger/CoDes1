e0000c037b821d725d2e98f219959dfcd698a391
LuigiPortugal <38053747+LuigiPortugal@users.noreply.github.com>
2018-09-25 17:27:11 -0300

    Firebase+layout 4X4(2x.img+2x.txt)

--- Java Script.js
+++ Java Script.js
@@ -5,6 +5,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
   let tpGoals = new TP('template-goals')
 
+  let tpGoals_1 = new TP('template-goals_1')
+  
+  let tpImg = new TP('template-img')
+
+  let tpImg_1 = new TP('template-img_1')
+
   let params = new URLSearchParams(window.location.search)
 
   let url = '/' + params.get('category') + '/' + params.get('course') + '/'
@@ -18,7 +24,16 @@ document.addEventListener('DOMContentLoaded', function() {
     let title = document.querySelector('.title')
     title.innerHTML = data['name']
 
-    let main_text = document.querySelector('.main_text')
-    main_text.innerHTML =  data['goals']
+    let main_text_left_txt = document.querySelector('.main_text_left_txt')
+    main_text_left_txt.innerHTML =  data['goals']
+
+    let main_text_right_txt1 = document.querySelector('.main_text_right_txt1')
+    main_text_right_txt1.innerHTML =  data['goals-1']
+
+    let main_text_left_jpg = document.querySelector('.main_text_left_jpg')
+    main_text_left_jpg.innerHTML =  data['img']
+
+     let main_text_right_jpg = document.querySelector('.main_text_right_jpg')
+    main_text_right_jpg.innerHTML =  data['img-1']
   })
 })
